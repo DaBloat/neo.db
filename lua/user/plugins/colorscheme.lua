6c766994ca35cf466f5afb97910e231ee4d9ee8a
DaBloat <kurtrusselvillamor1206@gmail.com>
2024-09-29 10:13:04 +0800

    Updates

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -4,6 +4,11 @@ return {
     priority = 1000,
     config = function()
       require("tokyonight").setup({
+	     on_colors = function(colors)
+		colors.bg = "#0C1717"
+		colors.bg_sidebar  = "#0C1717"
+		colors.bg_statusline = "#1B3434"
+	     end 
       })
       vim.cmd.colorscheme("tokyonight")
     end,
